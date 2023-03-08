e2a5183f358814ce1da957132d247048ae5d6cd3
elshora97 <ahmedashrafelshora@gmail.com>
2023-03-09 00:20:57 +0200

    Creating main Page and Sub Component

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,8 +1,31 @@
-import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
+import React, { useState, useEffect } from "react";
+import rgbToHex from "./utils";
 
-const SingleColor = () => {
-  return <h4>single color</h4>
-}
+const SingleColor = ({ rgb, weight, index }) => {
+  const [alert, setAlert] = useState(false);
+  const bgc = rgb.join(",");
+  const hex = rgbToHex(...rgb);
 
-export default SingleColor
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+      return clearTimeout(timeout);
+    }, 3000);
+  }, [alert]);
+  return (
+    <article
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hex);
+      }}
+      className={`color ${index > 10 && "color-light"}`}
+      style={{ backgroundColor: `rgb(${bgc})` }}
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">{hex}</p>
+      {alert && <p className="alert">Copied to Clipboar</p>}
+    </article>
+  );
+};
+
+export default SingleColor;
